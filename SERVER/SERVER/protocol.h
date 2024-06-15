6c956cbf89bf6deb31d5027682401867b4624576
ddoavoca <ddoavoca@gmail.com>
2024-06-15 15:48:12 +0900

    프로토콜 수정

--- SERVER/SERVER/protocol.h
+++ SERVER/SERVER/protocol.h
@@ -1,6 +1,8 @@
+#pragma once
 constexpr int PORT_NUM = 4000;
 constexpr int NAME_SIZE = 20;
 constexpr int CHAT_SIZE = 300;
+constexpr int BUF_SIZE = 200;   //++
 
 constexpr int MAX_USER = 10000;
 constexpr int MAX_NPC = 200000;
@@ -8,13 +10,17 @@ constexpr int MAX_NPC = 200000;
 constexpr int W_WIDTH = 2000;
 constexpr int W_HEIGHT = 2000;
 
+constexpr int SECTOR_SIZE = 10;      //++
+constexpr int SECTOR_COUNT_X = W_WIDTH / SECTOR_SIZE;   //++
+constexpr int SECTOR_COUNT_Y = W_HEIGHT / SECTOR_SIZE;   //++
+
 // Packet ID
 constexpr char CS_LOGIN = 0;
 constexpr char CS_MOVE = 1;
 constexpr char CS_CHAT = 2;
-constexpr char CS_ATTACK = 3;			// 4 방향 공격
-constexpr char CS_TELEPORT = 4;			// RANDOM한 위치로 Teleport, Stress Test할 때 Hot Spot현상을 피하기 위해 구현
-constexpr char CS_LOGOUT = 5;			// 클라이언트에서 정상적으로 접속을 종료하는 패킷
+constexpr char CS_ATTACK = 3;         // 4 방향 공격
+constexpr char CS_TELEPORT = 4;         // RANDOM한 위치로 Teleport, Stress Test할 때 Hot Spot현상을 피하기 위해 구현
+constexpr char CS_LOGOUT = 5;         // 클라이언트에서 정상적으로 접속을 종료하는 패킷
 
 constexpr char SC_LOGIN_INFO = 2;
 constexpr char SC_LOGIN_FAIL = 3;
@@ -23,91 +29,91 @@ constexpr char SC_REMOVE_OBJECT = 5;
 constexpr char SC_MOVE_OBJECT = 6;
 constexpr char SC_CHAT = 7;
 constexpr char SC_STAT_CHANGE = 8;
+constexpr char SC_LOGIN_OK = 9;   //++
 
 #pragma pack (push, 1)
 struct CS_LOGIN_PACKET {
 	unsigned short size;
-	char	type;
-	char	name[NAME_SIZE];
+	char   type;
+	char   name[NAME_SIZE];
 };
 
 struct CS_MOVE_PACKET {
 	unsigned short size;
-	char	type;
-	char	direction;  // 0 : UP, 1 : DOWN, 2 : LEFT, 3 : RIGHT
-	unsigned	move_time;
+	char   type;
+	char   direction;  // 0 : UP, 1 : DOWN, 2 : LEFT, 3 : RIGHT
+	unsigned   move_time;
 };
 
 struct CS_CHAT_PACKET {
-	unsigned short size;			// 크기가 가변이다, mess가 작으면 size도 줄이자.
-	char	type;
-	char	mess[CHAT_SIZE];
+	unsigned short size;         // 크기가 가변이다, mess가 작으면 size도 줄이자.
+	char   type;
+	char   mess[CHAT_SIZE];
 };
 
-struct CS_TELEPORT_PACKET {			// 랜덤으로 텔레포트 하는 패킷, 동접 테스트에 필요
+struct CS_TELEPORT_PACKET {
 	unsigned short size;
-	char	type;
+	char   type;
 };
 
 struct CS_LOGOUT_PACKET {
 	unsigned short size;
-	char	type;
+	char   type;
 };
 
 struct SC_LOGIN_INFO_PACKET {
 	unsigned short size;
-	char	type;
-	int		visual;				// 종족, 성별등을 구분할 때 사용
-	int		id;
-	int		hp;		// ui 에만 표시
-	int		max_hp;
-	int		exp;
-	int		level;
-	short	x, y;
+	char   type;
+	int      id;
+	int      hp;
+	int      max_hp;
+	int      exp;
+	int      level;
+	short   x, y;
 };
 
 struct SC_ADD_OBJECT_PACKET {
 	unsigned short size;
-	char	type;
-	int		id;
-	int		visual;				// 어떻게 생긴 OBJECT인가를 지시
-	short	x, y;
-	char	name[NAME_SIZE];
+	char   type;
+	int      id;
+	short   x, y;
+	char   name[NAME_SIZE];
 };
 
 struct SC_REMOVE_OBJECT_PACKET {
 	unsigned short size;
-	char	type;
-	int		id;
+	char   type;
+	int      id;
 };
 
 struct SC_MOVE_OBJECT_PACKET {
 	unsigned short size;
-	char	type;
-	int		id;
-	short	x, y;
-	unsigned int move_time;	// 스트레스 테스트에서 사용하는 것
+	char   type;
+	int      id;
+	short   x, y;
+	unsigned int move_time;
 };
 
 struct SC_CHAT_PACKET {
 	unsigned short size;
-	char	type;
-	int		id;
-	char	mess[CHAT_SIZE];
+	char   type;
+	int      id;
+	char   mess[CHAT_SIZE];
 };
 
 struct SC_LOGIN_FAIL_PACKET {
 	unsigned short size;
-	char	type;
+	char   type;
+
 };
 
-struct SC_STAT_CHANGE_PACKET {
+struct SC_STAT_CHANGEL_PACKET {
 	unsigned short size;
-	char	type;
-	int		hp;
-	int		max_hp;
-	int		exp;
-	int		level;
+	char   type;
+	int      hp;
+	int      max_hp;
+	int      exp;
+	int      level;
 
 };
 
