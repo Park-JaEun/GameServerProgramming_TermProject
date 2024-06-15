1530304f5821bf9a1bdce97dbdee2d706fcab0d6
ddoavoca <ddoavoca@gmail.com>
2024-06-15 15:46:32 +0900

    npc 스크립트

---
+++ SERVER/SERVER/npc.lua
@@ -0,0 +1,21 @@
+myid = 99999;
+
+function set_uid(x)
+   myid = x;
+end
+
+function event_player_move(player)
+   player_x = API_get_x(player);
+   player_y = API_get_y(player);
+   my_x = API_get_x(myid);
+   my_y = API_get_y(myid);
+   my_move_time = API_get_npc_move_time(myid);
+   if (player_x == my_x) then
+      if (player_y == my_y) then
+         API_SendMessage(myid, player, "HELLO");
+      end
+   end
+   if(my_move_time == 3) then
+      API_SendMessage(myid, player, "BYE");
+   end
+  end
